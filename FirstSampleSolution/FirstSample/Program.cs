523c573cb1d493bb14cf784b0333c847a4ed6aaf
rshibu1 <77075453+rshibu1@users.noreply.github.com>
2021-01-13 10:56:15 -0700

    Sample

--- FirstSampleSolution/FirstSample/Program.cs
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -12,7 +12,7 @@ namespace FirstSample
             Console.WriteLine("This course section is:\t OA03 ");
             Console.WriteLine("Your instructor name is:\t Don Welch");
 
-            string myName = "Don Welch";
+            string myName = "Roshan Shibu";
             Console.WriteLine($"This name is printed from a variable is:\t{myName}");
         }
     }
